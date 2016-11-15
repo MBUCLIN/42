496c98474a9d48d034137c1ebedcf8f326dad61c
Matthieu BUCLIN <mbuclin@e1r5p18.42.fr>
2016-11-15 17:10:03 +0100

    edit command need to search last bugs

--- 21sh/src/command/handle_stnd.c
+++ 21sh/src/command/handle_stnd.c
@@ -6,27 +6,11 @@
 /*   By: mbuclin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 14:59:19 by mbuclin           #+#    #+#             */
-/*   Updated: 2016/11/14 15:36:22 by mbuclin          ###   ########.fr       */
+/*   Updated: 2016/11/15 17:05:21 by mbuclin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
-
-static int	get_moducursor(int cursor)
-{
-	int		col;
-
-	col = tgetnum("co");
-	return (cursor % col);
-}
-
-static int	get_line(int cursor)
-{
-	int		col;
-
-	col = tgetnum("co");
-	return (cursor / col);
-}
 
 void		handle_stnd(char *buf, t_command **cmd)
 {
@@ -34,7 +18,7 @@ void		handle_stnd(char *buf, t_command **cmd)
 	int		up;
 	int		line_len;
 
-	mv = get_moducursor(get_cursor(LENGT, cmd));
+	mv = get_column(get_cursor(LENGT, cmd));
 	up = get_line(get_cursor(LOCAT, cmd));
 	line_len = get_line(get_cursor(LENGT, cmd));
 	if (buf[2] == 72)
